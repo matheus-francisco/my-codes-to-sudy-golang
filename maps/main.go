06e41cae96d836484fdbc9dc8a394f5f5bba5142
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-08-18 09:35:37 -0300

    Starting study about map in go

---
+++ maps/main.go
@@ -0,0 +1,19 @@
+
+package main
+
+import "fmt"
+
+func main(){
+  //maps in go
+  // all value the same type!
+  // key -> value
+  // key -> value
+
+  colors := map[string]string {
+    "red": "#ff0000",
+    "green": "#4bf745",
+  }
+
+  fmt.Println(colors)
+
+}

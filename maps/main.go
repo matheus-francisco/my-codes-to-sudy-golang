8e69c831f1c190281fd809eafa085e81a7c52544
matheusfrancisco <matheusmachadoufsc@gmail.com>
2020-08-20 22:47:37 -0300

    types maps in golang

--- maps/main.go
+++ maps/main.go
@@ -9,11 +9,29 @@ func main(){
   // key -> value
   // key -> value
 
-  colors := map[string]string {
-    "red": "#ff0000",
-    "green": "#4bf745",
+   colors := map[string]string {
+     "red": "#ff0000",
+     "green": "#4bf745",
+   }
+  //colors := make(map[string]string)
+  //colors_int := make(map[int]string)
+
+  //colors["white"] ="#ffffff"
+  //colors_int[10] ="#ffffff"
+
+  //fmt.Println(colors)
+  //fmt.Println(colors_int)
+
+  //delete(colors_int, 10)
+  //delete(colors, "white")
+
+  //fmt.Println(colors)
+  //fmt.Println(colors_int)
+  printMap(colors)
+}
+
+func printMap(c map[string]string) {
+  for color, hex := range c {
+      fmt.Println("Hex code for", color, "is", hex)
   }
-
-  fmt.Println(colors)
-
 }
